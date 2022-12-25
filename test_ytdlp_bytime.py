b0c80e13e8ca8ce2fc61fdec4a8d3eee900c6aa4
IwamotoKakeru <kganpipi@gmail.com>
2022-12-25 23:53:58 +0900

    Changed to treat as a module

--- test_ytdlp_bytime.py
+++ test_ytdlp_bytime.py
@@ -1,12 +1,12 @@
-import ytdlp_bytime as yt_bt
+import ytdlpbytime.download as dl_bytime
 
 
 def main():
-    movie_url: str = 'https://www.youtube.com/watch?v=6T8Jia_4tZc'
+    movie_url: str = 'https://www.youtube.com/watch?v=v0ZQU2gq_J0'
     start_time: int = 10
     end_time: int = 45
 
-    yt_bt.Download.movie(movie_url, start_time, end_time)
+    dl_bytime.movie(movie_url, start_time, end_time)
 
 
 if __name__ == "__main__":
---
+++ ytdlpbytime/download.py
@@ -0,0 +1,20 @@
+from yt_dlp import YoutubeDL
+
+
+def movie(movie_url: str, start_time: int, end_time: int):
+
+    def set_download_ranges(info_dict, self):
+        duration_opt = [{
+            'start_time': start_time,
+            'end_time': end_time
+        }]
+        return duration_opt
+
+    ydl_opts = {
+        'download_ranges': set_download_ranges
+    }
+
+    movieURL: str = movie_url
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download(movieURL)
