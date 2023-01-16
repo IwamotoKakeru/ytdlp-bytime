069f8bb2a8e17ce32a2f67819d94e83e342ad744
IwamotoKakeru <kganpipi@gmail.com>
2023-01-16 22:46:11 +0900

    Add sound func

--- ytdlpbytime/download.py
+++ ytdlpbytime/download.py
@@ -18,3 +18,23 @@ def movie(movie_url: str, start_time: int, end_time: int):
 
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download(movieURL)
+
+
+def sound(movie_url: str, start_time: int, end_time: int):
+
+    def set_download_ranges(info_dict, self):
+        duration_opt = [{
+            'start_time': start_time,
+            'end_time': end_time
+        }]
+        return duration_opt
+
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        'download_ranges': set_download_ranges
+    }
+
+    movieURL: str = movie_url
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download(movieURL)
